0db656a469fd3571200dbb5ce78d307edbbe82ca
wynnemlo <wynnemlo@gmail.com>
2014-10-25 13:59:16 +0800

    initial commit

---
+++ tictactoe.rb
@@ -0,0 +1,13 @@
+Two players take turns to make a move, either X or O, on a board
+that consists of 9 squares. The first one to reach a line wins the game.
+
+class Board
+end
+class Player
+end
+class User < Player
+end
+class Computer < Player
+end
+class Game
+end
